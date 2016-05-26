efb11160e7dcf3cb41147a3a5891dc9588f345bd
Antoine Drouhin <antoine.drouhin@gmail.com>
2016-05-26 16:47:20 +0200

    HashOAT.ajouter()

--- AlgoAvTD4/src/algoavtd4/HashOAT.java
+++ AlgoAvTD4/src/algoavtd4/HashOAT.java
@@ -9,23 +9,54 @@ package algoavtd4;
  *
  * @author aureliengarret
  */
+public class HashOAT<V> extends HashDico<V> {
 
-public class HashOAT<V> extends HashDico<V>{
-    
     private Element[] tab;
-    
+
     public HashOAT(int tailleInit) {
         this.tab = (Element[]) new Object[tailleInit];
+        nbElem = 0;
     }
-    
+
     public HashOAT() {
         this(32);
     }
-    
+
     @Override
     public V ajouter(String cle, V valeur) {
+
+        int i = Math.abs(hashString(cle))%tab.length;
+        V retour = null;
         
-        int hash = hashString(cle);
+        if (tab[i] == null) {
+            tab[i] = new Element(cle, valeur);
+        }
+        else {
+            while (true) {
+                if (tab[++i%tab.length] == null) {
+                    tab[i] = new Element(cle, valeur);
+                    break;
+                }
+                if (tab[i].libre) {
+                    tab[i].cle = cle;
+                    tab[i].valeur = valeur;
+                    tab[i].libre = false;
+                    break;
+                }
+                if (tab[i].cle.equals(cle)){
+                    retour = tab[i].valeur;
+                    tab[i].valeur = valeur;
+                    break;
+                }
+            }
+        }
+        
+        compteurTaille++;
+        
+        if(!estOrganise())
+            organiser();
+        
+        return retour;
         
     }
 
@@ -61,7 +92,7 @@ public class HashOAT<V> extends HashDico<V>{
 
     @Override
     public boolean estOrganise() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return nbElem > 0.75 * tab.length;
     }
 
     @Override
@@ -74,15 +105,17 @@ public class HashOAT<V> extends HashDico<V>{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private class Element{
-        String cle;
-        V valeur;
-        boolean libre;
-        
-        Element(String cle, V valeur){
-            this.cle = cle;
-            this.valeur = valeur;
-            libre = false;
-        }
+    private class Element {
+
+    String cle;
+    V valeur;
+    boolean libre;
+
+    Element(String cle, V valeur) {
+        this.cle = cle;
+        this.valeur = valeur;
+        libre = false;
+    }
+
     }
 }
