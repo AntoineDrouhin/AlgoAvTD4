218ca328d8ac9de0cd29ba778f0ae93106a6c31f
Aurelien Garret <aurelien.garret@gmail.com>
2016-05-31 21:22:55 +0200

    estVide hashChainee

--- AlgoAvTD4/src/algoavtd4/HashChainee.java
+++ AlgoAvTD4/src/algoavtd4/HashChainee.java
@@ -126,7 +126,7 @@ public class HashChainee<V> extends HashDico<V> {
 
     @Override
     public boolean estVide() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return nbElem == 0;
     }
 
     @Override
